6c35612d9ac794aa1d69dc39673478c78cea6d69
Hasan <hasanm9794@gmail.com>
2020-12-20 21:55:02 +0500

    Converted to ant UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,37 +8,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => (
-  <div className="App">
-    <Row>
-      <Col span={12} offset={0}>Hello</Col>
-      <Col span={12} offset={15}>
-        <Card style={{ width: 300 }}>
-          <Row className="userIDRow">
-            <Col span={2}>
-              <p className="userID">UserID</p>
-            </Col>
-            <Col span={18}></Col>
-            <Input />
-          </Row>
+	<div className="App">
+		<Row>
+			<Col span={12} offset={0}>Hello</Col>
+			<Col span={12} offset={15}>
+				<Row>
+					<Col span={10}><h1 className="loginText">LOGIN</h1></Col>
+				</Row>
+				<Row>
+					<Card className="loginBox" style={{ width: 300 }}>
+						<Row className="userIDRow">
+							<Col span={2}>
+								<p className="userID">UserID</p>
+							</Col>
+							<Col span={18}></Col>
+							<Input className="inp1" />
+						</Row>
+						<Row className="passwordRow">
+							<Col span={2}><p className="password">Password</p></Col>
+							<Col span={18}></Col>
+							<Input className="inp1" />
+						</Row>
+						<Row className="btnRow">
+							<Col span={24}>
+								<Button type="primary" shape="round" size="large">Signin</Button>
+								<p></p>
+								<p className="forgot">forgot password?</p>
+							</Col>
+						</Row>
+					</Card>
+				</Row>
 
-          <Row className="passwordRow">
-            <Col span={2}><p className="password">Password</p></Col>
-            <Col span={18}></Col>
-            <Input />
-          </Row>
-          <Row className="btnRow">
-            <Col span={24}>
-              <Button type="primary" shape="round" size="large">Signin</Button>
-              <p></p>
-          <p className="forgot">forgot password?</p>
-            </Col>
-          </Row>
-          
-
-        </Card>,
-      </Col>
-    </Row>
-  </div>
+			</Col>
+		</Row>
+	</div>
 );
 
 export default App;
