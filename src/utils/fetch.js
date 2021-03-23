3c342ec0f8497d033636b9e79419456fc43e2939
Misbah Khalil <misbah.khalil.az@gmail.com>
2021-03-23 14:27:16 +0500

    formdata support on fetch module

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,18 +1,18 @@
-const API = (method, end_point, body, token = "") => {
+const API = (method, end_point, body=null, token = "", formData=null) => {  //for formdata, API('post', endpVal, null, tokenStr, formDataObject)
   const requestOptions = {
     method,
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token ? token : ""}`,
     },
-    body: JSON.stringify(body),
+    body: body ? JSON.stringify(body) : formData,
     redirect: "follow",
   };
 
   return fetch(
     `https://lms-fyp-devs.herokuapp.com${end_point}`,
     requestOptions
-  ).then((res) => res.json().then((data) => ({ status: res.status, data })));
-  // .catch((err) => console.log(err, "CAtCH"));
+  ).then((res) => res.json().then((data) => ({ status: res.status, data })))
+  .catch((err) => console.log(err));    //catch must be there
 };
 export default API;
