21849e9efb59e84201fb5167bdc0b0ac078798bf
Misbah Khalil <misbah.khalil.az@gmail.com>
2021-04-01 00:06:10 +0500

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React, { useEffect } from "react";
 import Router from "./Router/Router";
 import "./styles/App.css";
 import { clearStoreAction } from "./redux/actions/LoggerActions";
+import ChatMain from "./components/chat-main"
 
 const App = () => {
   useEffect(() => () => clearStoreAction(), []);
 
-  return <Router />;
+  return <ChatMain />;
 };
 
 export default App;
